3accc5c28ba0e9534af800e8aa70c6bdc6bae63e
Alfa <travaotilona@gmail.com>
2024-06-20 12:13:22 +0300

    Memory display

--- TaskMeneger/TaskMeneger/MainForm.Designer.cs
+++ TaskMeneger/TaskMeneger/MainForm.Designer.cs
@@ -36,8 +36,13 @@
 			this.tabPage2 = new System.Windows.Forms.TabPage();
 			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
 			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.tabControl.SuspendLayout();
 			this.tabPageProcesses.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tabControl
@@ -47,10 +52,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.tabControl.Controls.Add(this.tabPageProcesses);
 			this.tabControl.Controls.Add(this.tabPage2);
-			this.tabControl.Location = new System.Drawing.Point(0, 12);
+			this.tabControl.Location = new System.Drawing.Point(0, 31);
 			this.tabControl.Name = "tabControl";
 			this.tabControl.SelectedIndex = 0;
-			this.tabControl.Size = new System.Drawing.Size(800, 413);
+			this.tabControl.Size = new System.Drawing.Size(800, 394);
 			this.tabControl.TabIndex = 0;
 			// 
 			// tabPageProcesses
@@ -59,7 +64,7 @@
 			this.tabPageProcesses.Location = new System.Drawing.Point(4, 25);
 			this.tabPageProcesses.Name = "tabPageProcesses";
 			this.tabPageProcesses.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPageProcesses.Size = new System.Drawing.Size(792, 384);
+			this.tabPageProcesses.Size = new System.Drawing.Size(792, 365);
 			this.tabPageProcesses.TabIndex = 0;
 			this.tabPageProcesses.Text = "Processes";
 			this.tabPageProcesses.UseVisualStyleBackColor = true;
@@ -72,18 +77,17 @@
 			this.listViewProcesses.Location = new System.Drawing.Point(3, 3);
 			this.listViewProcesses.MultiSelect = false;
 			this.listViewProcesses.Name = "listViewProcesses";
-			this.listViewProcesses.Size = new System.Drawing.Size(786, 378);
+			this.listViewProcesses.Size = new System.Drawing.Size(786, 359);
 			this.listViewProcesses.TabIndex = 0;
 			this.listViewProcesses.UseCompatibleStateImageBehavior = false;
 			this.listViewProcesses.View = System.Windows.Forms.View.Details;
-			//this.listViewProcesses.SelectedIndexChanged += new System.EventHandler(this.listViewProcesses_SelectedIndexChanged);
 			// 
 			// tabPage2
 			// 
 			this.tabPage2.Location = new System.Drawing.Point(4, 25);
 			this.tabPage2.Name = "tabPage2";
 			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(792, 384);
+			this.tabPage2.Size = new System.Drawing.Size(792, 365);
 			this.tabPage2.TabIndex = 1;
 			this.tabPage2.Text = "tabPage2";
 			this.tabPage2.UseVisualStyleBackColor = true;
@@ -103,18 +107,55 @@
 			this.timer1.Interval = 1000;
 			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
 			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+			this.menuStrip1.TabIndex = 2;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// runToolStripMenuItem
+			// 
+			this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+			this.runToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+			this.runToolStripMenuItem.Text = "Run";
+			this.runToolStripMenuItem.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+			this.exitToolStripMenuItem.Text = "Exit";
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
 			this.Controls.Add(this.tabControl);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "MainForm";
 			this.Text = "TaskManager";
 			this.tabControl.ResumeLayout(false);
 			this.tabPageProcesses.ResumeLayout(false);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -129,6 +170,10 @@
         private ListViewSmooth listViewProcesses;
         //private System.Windows.Forms.ListView listViewProcesses;
 		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
 	}
 }
 
--- TaskMeneger/TaskMeneger/MainForm.cs
+++ TaskMeneger/TaskMeneger/MainForm.cs
@@ -13,7 +13,9 @@ namespace TaskMeneger
 {
     public partial class MainForm : Form
     {
-        Dictionary<int, Process> d_processes;
+		readonly int ramFactor = 1024;
+		readonly string suffix = "kB";
+		Dictionary<int, Process> d_processes;
         public MainForm()
         {
             InitializeComponent();
@@ -27,12 +29,16 @@ namespace TaskMeneger
             //LoadProcesses();
             AddNewProcesses();
             RemoveOldProcesses();
+            UpdateExistingProcesses();
 			statusStrip1.Items[0].Text = ($"Количество процессов: {listViewProcesses.Items.Count}");
 		}
         void SetColumns()
         {
             listViewProcesses.Columns.Add("PID");
             listViewProcesses.Columns.Add("Name");
+            listViewProcesses.Columns.Add("Working set");
+            listViewProcesses.Columns.Add("Peak working set");
+            
         }
         void LoadProcesses()
         {
@@ -47,11 +53,8 @@ namespace TaskMeneger
             //}
             d_processes = Process.GetProcesses().ToDictionary(item => item.Id, item => item);
             foreach(KeyValuePair<int, Process> i in d_processes)
-            {
-                ListViewItem item = new ListViewItem();
-                item.Text = i.Key.ToString();
-                item.SubItems.Add(i.Value.ProcessName);
-                listViewProcesses.Items.Add(item); 
+            {               
+                AddProcessToListView(i.Value);
             }
             //statusStrip1.Items[0].Text = ($"Количество процессов: {listViewProcesses.Items.Count}");
         }
@@ -78,16 +81,35 @@ namespace TaskMeneger
                     listViewProcesses.Items.RemoveAt(i);
             }
         }
+        void UpdateExistingProcesses()
+        {
+            for(int i=0; i<listViewProcesses.Items.Count; i++)
+            {
+                int id = Convert.ToInt32(listViewProcesses.Items[i].Text);
+                //Process process = d_processes[id];
+                listViewProcesses.Items[i].SubItems[2].Text = $"{d_processes[id].WorkingSet64 / ramFactor} {suffix}";
+                listViewProcesses.Items[i].SubItems[3].Text = $"{d_processes[id].PeakWorkingSet64 / ramFactor} {suffix}";
+            }
+        }
         void AddProcessToListView(Process process)
         {
+            
             ListViewItem item = new ListViewItem();
             item.Text = process.Id.ToString();
             item.SubItems.Add(process.ProcessName);
+            item.SubItems.Add($"{process.WorkingSet64/ramFactor} {suffix}");
+            item.SubItems.Add($"{process.PeakWorkingSet64/ramFactor} {suffix}"); 
+            //item.SubItems.Add($"{process.} {suffix}"); 
             listViewProcesses.Items.Add(item);
         }
         void RemoveProcessFromListView(int pid)
         {
             listViewProcesses.Items.RemoveByKey(pid.ToString());
         }
+
+		private void runToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
